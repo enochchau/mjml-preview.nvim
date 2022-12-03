be5df39a0dd2bbe14887ad56f4b338c5e1e5c8a8
ec965 <22567131+ec965@users.noreply.github.com>
2022-12-03 15:18:14 -0800

    Update health check to display version error

--- lua/mjml-preview/health.lua
+++ lua/mjml-preview/health.lua
@@ -4,18 +4,16 @@ M.check = function()
 	vim.health.report_start("Checking external dependencies")
 
 	if vim.fn.executable("node") ~= 1 then
-		vim.health.report_error("NodeJS is not installed. NodeJS 14 must be installed for this plugin to work.")
+		vim.health.report_error("NodeJS is not installed. NodeJS v14 and greater must be installed for this plugin to work.")
 		return
 	end
 
-	local handle = io.popen("node --version")
+	local full_version = vim.fn.trim(vim.fn.system("node --version"))
+	vim.health.report_info("Found NodeJS " .. full_version)
 
-	if handle ~= nil then
-		local binary_version = handle:read("*a")
-		handle:close()
-		binary_version = binary_version:gsub("%s+", "")
-
-		vim.health.report_info("Found NodeJS " .. binary_version)
+	local primary_version = full_version:match("(%d+).(%d+).(%d+)")
+	if tonumber(primary_version) < 14 then
+		vim.health.report_error("Minimum version not met, this plugin requires v14 and greater")
 	end
 end
 
